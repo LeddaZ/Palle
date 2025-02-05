b9bd0ba9e5c133759893e1cae63dd8256116b39d
Leonardo Ledda <leonardoledda@gmail.com>
2025-02-05 12:41:11 +0100

    Implement lives
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,8 +7,11 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
+    public GameObject ball;
     public Button restartButton;
     private int score = 0;
+    private int lives = 3;
 
     void Awake()
     {
@@ -17,7 +20,6 @@ public class ScoreManager : MonoBehaviour
             instance = this;
         gameOverText.gameObject.SetActive(false); // Hide game over text
         restartButton.gameObject.SetActive(false); // Hide restart button
-
     }
 
     public void AddPoint()
@@ -28,8 +30,17 @@ public class ScoreManager : MonoBehaviour
 
     public void GameOver()
     {
-        gameOverText.gameObject.SetActive(true); // Show game over text
-        restartButton.gameObject.SetActive(true); // Show restart button
-        Time.timeScale = 0;
+        if (lives >= 1)
+        {
+            lives--; // Decrease lives
+            livesText.text = "Lives: " + lives; // Update UI text
+            ball.gameObject.transform.position = new Vector3(3.7f, 1.6f, 0.03f); // Reset ball position
+        }
+        else
+        {
+            gameOverText.gameObject.SetActive(true); // Show game over text
+            restartButton.gameObject.SetActive(true); // Show restart button
+            Time.timeScale = 0;
+        }
     }
 }
